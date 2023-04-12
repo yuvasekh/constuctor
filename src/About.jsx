21e9ccc1641e870d90161bbffb368fc4067cf798
Yuva Sekhar <syuva893@gmail.com>
2023-04-12 20:27:35 +0530

    changees

--- src/About.jsx
+++ src/About.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
  const About = () => {
   return (
-    <div>Services</div>
+ 
+<div className="row">
+  <div className="column">
+    <div className="card">
+      <h3>Card 1</h3>
+      <p>Some text</p>
+      <p>Some text</p>
+    </div>
+  </div>
+
+  <div className="column">
+    <div className="card">
+      <h3>Card 2</h3>
+      <p>Some text</p>
+      <p>Some text</p>
+    </div>
+  </div>
+</div>
   )
 }
 export default About
